174fb683a77071186067bfa2d8650aac0fa4ea22
Oier <oieruni22@gmail.com>
2021-10-24 23:55:34 +0200

    Empezando el proyecto

---
+++ aplicacion_server.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import socket
+PORT = 50001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+s.bind(('', 50001))
+
+while True:
+    mensaje, dir_cli = s.recvfrom(1024)
+
+    s.sendto(mensaje, dir_cli)
+s.close()
