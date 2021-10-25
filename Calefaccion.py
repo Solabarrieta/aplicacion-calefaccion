1d9ee5f40d25d0c3cffbfb5da834a2959db0d125
samuelguzmanajala <samuelguzmanajala@gmail.com>
2021-10-25 11:26:45 +0200

    avanze

--- Calefaccion.py
+++ Calefaccion.py
@@ -1,4 +1,9 @@
 class Calefaccion:
-    def __init__(self, id, status):
+    def __init__(self, id, nombre, status,temperatura):
         self.id=id
+        self.nombre=nombre
         self.status=status
+        self.temperatura=temperatura
+    def toString(self):
+        s = "{},{}".format(self.id,self.nombre)
+        return s
--- aplicacion_server.py
+++ aplicacion_server.py
@@ -7,10 +7,10 @@ PORT = 50001
 
 
 
-calefaccion1=Calefaccion('1',False)
-calefaccion2=Calefaccion('2',False)
-calefaccion3=Calefaccion('3',False)
-calefaccion4=Calefaccion('4',False)
+calefaccion1=Calefaccion('1','sala',False,25)
+calefaccion2=Calefaccion('2','cocina',False,18)
+calefaccion3=Calefaccion('3','habitacion',False,30)
+calefaccion4=Calefaccion('4','baño',False,27)
 calefacciones=[calefaccion1,calefaccion2,calefaccion3,calefaccion4]
 
 
@@ -26,11 +26,11 @@ while True:
     parametros=mensaje[3:len(mensaje)].split(':')
     if(comando=="ONN"):
         print("ha usado el comando ONN")
-        if(len(parametros)==1):
+        print(parametros)
+        if(parametros==['']):
             for calefaccion in calefacciones:
                 calefaccion.status=True
         else:
-            print("else")
             for calefaccion in calefacciones:
                 for parametro in parametros:
                     if(parametro==calefaccion.id):
@@ -39,14 +39,38 @@ while True:
             print(calefaccion.status)
     if(comando=="OFF"):
         print("ha usado el comando OFF")
+        if(parametros==['']):
+            for calefaccion in calefacciones:
+                calefaccion.status=False
+        else:
+            for calefaccion in calefacciones:
+                for parametro in parametros:
+                    if(parametro==calefaccion.id):
+                        calefaccion.status=False
+        for calefaccion in calefacciones: 
+            print(calefaccion.status)
     if(comando=="NAM"):
         print("ha usado el comando NAM")
+        if(parametros==['']):
+            sol="{}".format(calefacciones[0].toString())
+            for i in range(1,len(calefacciones)):
+                sol="{}:{}".format(sol,calefacciones[i].toString())
+                print(sol)
     if(comando=="NOW"):
         print("ha usado el comando NOW")
+        if(parametros==['']):
+            for calefaccion in calefacciones:
+                print('id:',calefaccion.id,'temperatura:',calefaccion.temperatura)
+        else:
+            for calefaccion in calefacciones:
+                for parametro in parametros:
+                    if(parametro==calefaccion.id):
+                        print('id:',calefaccion.id,'temperatura:',calefaccion.temperatura)
+
     if(comando=="GET"):
         print("ha usado el comando GET")
     if(comando=="SET"):
         print("ha usado el comando SET")
     
-    s.sendto(mensaje, dir_cli)
+    s.sendto(sol.encode(), dir_cli)
 s.close()
